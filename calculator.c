067e1a5edf9b67daa2a8cfea2e33ec77494d02e2
ImAwsum <153707128+ImAwsumm@users.noreply.github.com>
2025-02-23 16:27:21 -0500

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -84,6 +84,34 @@ void on_calculate_button_clicked(GtkWidget *widget, gpointer data) {
     gtk_label_set_text(GTK_LABEL(gtk_widget_get_parent(widget)), result_text);
 }
 
+// Function to create the main window
+void create_window() {
+    GtkWidget *window, *grid, *entry, *button, *label;
+
+    gtk_init(NULL, NULL);
+    
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Text Calculator");
+    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(window), grid);
+
+    entry = gtk_entry_new();
+    gtk_grid_attach(GTK_GRID(grid), entry, 0, 0, 1, 1);
+
+    button = gtk_button_new_with_label("Calculate");
+    g_signal_connect(button, "clicked", G_CALLBACK(on_calculate_button_clicked), entry);
+    gtk_grid_attach(GTK_GRID(grid), button, 0, 1, 1, 1);
+
+    label = gtk_label_new("Result: ");
+    gtk_grid_attach(GTK_GRID(grid), label, 0, 2, 1, 1);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+}
+
 int main(int argc, char *argv[]) {
     create_window();
     return 0;
